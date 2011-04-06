d3bbff1fbdcf0f6654aa72ce1ee31ed173d81fc4
James A. Rosen and Nathan Rapheld <dev+james+nathan@zendesk.com>
2011-04-06 13:55:33 -0700

    Added Jasmine, Jasmine-jQuery, and first spec

---
+++ spec/CoreSpec.js
@@ -0,0 +1,13 @@
+describe('flapjack', function() {
+
+  var fixture = "";
+
+  beforeEach(function() {
+    setFixtures(fixture);
+  });
+
+  it('should exist', function() {
+    expect(window.Flapjack).not.toBeUndefined();
+  });
+
+});
